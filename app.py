eae26cbcd0822373e2ba3d073b056129d3835334
Sean Blum <seanblum@gmail.com>
2025-05-01 17:39:31 -0700

    integrate with alpha vantage

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import os
+import requests
 import time
+from pprint import pprint
 from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+alpha_vantage_api_key = os.environ["ALPHA_VANTAGE_API_KEY"]
 test_number = os.environ["TEST_NUMBER"]
 client = Client(account_sid, auth_token)
 
-# send test message to Sean
+# alpha vantage
+SYMBOL = "PLTR"
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={SYMBOL}&interval=5min&apikey={alpha_vantage_api_key}'
+response = requests.get(url)
+data = response.json()
+pprint(data)
+start = ...
+end = ...
+direction = "up" if (end - start) > 0 else "down"
+message = f"Palantir is {direction} today! It started at {start} and ended at {end}."
+
+# send test message to test number
 message = client.messages.create(
   from_='whatsapp:+14155238886',
-  body="the market is terrible today! switch to bonds!",
+  body=message,
   to=f"whatsapp:{test_number}"
 )
 print(message.status)
